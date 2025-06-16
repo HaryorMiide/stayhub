7e917a5eef9c60fe3ff368845657d27be87ae073
HaryorMiide <haryormiidesanusi@gmail.com>
2025-06-16 09:54:06 +0100

    app test

--- src/test/java/com/stayhub/Stayhub/StayhubApplicationTests.java
+++ src/test/java/com/stayhub/Stayhub/StayhubApplicationTests.java
@@ -2,7 +2,9 @@ package com.stayhub.Stayhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")  // ✅ This tells Spring to load application-test.properties
 @SpringBootTest
 class StayhubApplicationTests {
 
